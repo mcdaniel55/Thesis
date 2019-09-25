27fe58dae564b99305e10e2dfed2b8411369a63c
Kevin McDaniel <amygdaloid733@gmail.com>
2019-09-24 21:45:37 -0400

    Initial commit

---
+++ Thesis/Thesis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Thesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
