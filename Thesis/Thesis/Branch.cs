0632289ff943b17cf9f56f40f84e8c4aeb88818c
Kevin McDaniel <mcdaniel55@live.marshall.edu>
2019-10-29 22:10:11 -0400

    Began integrating the full optimization test code.

---
+++ Thesis/Thesis/Branch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using MathNet.Numerics.Distributions;
+
+namespace Thesis
+{
+    /// <summary> Encapsulates a subset of the solution space that can be branched, and on collections of which a bounding operation can be performed </summary>
+    abstract class Branch
+    {
+        #region Properties
+        /// <summary> Keeps track of how many times the solution space was branched to produce this subset </summary>
+        public int BranchLevel { get; private set; }
+        public IContinuousDistribution SamplingDistribution { get; private set; }
+        /// <summary> The best (smallest) observation of the fitness function </summary>
+        public double MinimumObservedFitness { get; private set; }
+        public object BestObservedSolution { get; private set; }
+        public readonly Random m_rand;
+        #endregion
+
+        #region Methods
+        /// <summary> Obtains a single value at random from this region </summary>
+        protected abstract double Sample();
+
+        /// <summary> Creates a cover of this subset according to problem-specific structure </summary>
+        public abstract Branch[] GetBranches();
+
+        public virtual void Samples(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Sample();
+            }
+        }
+
+        /// <summary> A constructor that specifies some common initialization logic and 
+        /// provides an explicit parameterization of all subclass constructors </summary>
+        protected Branch(Random rand)
+        {
+            m_rand = rand ?? Program.rand;
+            MinimumObservedFitness = double.PositiveInfinity;
+        }
+
+        #endregion
+    }
+}
---
+++ Thesis/Thesis/IntervalBranch.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thesis
+{
+    class IntervalBranch : Branch
+    {
+        /// <summary> The infimum of the interval </summary>
+        public double Start { get; private set; }
+        /// <summary> The supremum of the interval </summary>
+        public double End { get; private set; }
+        /// <summary> The range of the values contained in the interval </summary>
+        private double range;
+
+        /// <summary>
+        /// Creates a new interval with the designated start and end points
+        /// </summary>
+        /// <param name="start"> A finite number less than end </param>
+        /// <param name="end"> A finite number greater than start </param>
+        /// <param name="rand"> An override for the default random number generator </param>
+        public IntervalBranch(double start, double end, Random rand = null) : base(rand)
+        {
+            Start = start;
+            End = end;
+            range = end - start;
+        }
+
+        public override Branch[] GetBranches()
+        {
+            // Bisection version
+            double midPoint = (End + Start) / 2;
+            return new Branch[] { new IntervalBranch(Start, midPoint, m_rand),
+                new IntervalBranch(midPoint, End, m_rand) };
+
+
+
+            // Trisection version
+            /*
+            double mid1 = Start + range / 3;
+            double mid2 = Start + 2 * range / 3;
+            return new Branch[] { new IntervalBranch(Start, mid1, m_rand),
+                new IntervalBranch(mid1, mid2, m_rand),
+                new IntervalBranch(mid2, End, m_rand) };
+            */
+
+            // Quartering version
+            /*
+            double q1 = start + range / 4;
+            double q2 = start + range / 2;
+            double q3 = start + 3 * range / 4;
+            return new Branch[] { new IntervalBranch(start, q1, m_rand),
+                new IntervalBranch(q1, q2, m_rand),
+                new IntervalBranch(q2, q3, m_rand),
+                new IntervalBranch(q3, end, m_rand) };
+            */
+        }
+
+        protected override double Sample()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
